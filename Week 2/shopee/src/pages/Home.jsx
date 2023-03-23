a9ac39bb0d0c82921344d123da014c6a2d4ba7b0
unknown <phamhuutinh801@gmail.com>
2023-03-23 15:09:21 +0700

    week 2

---
+++ Week 2/shopee/src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import Header from "../components/Header";
+import ProductList from "../components/ProductList";
+import Footer from "../components/Footer";
+import Carousel from "../components/Carousel";
+import { CartState } from "../context/Context";
+
+
+const Home = () => {
+  const {
+    state: { products },
+    productState: {searchQuery },
+  } = CartState();
+  const handleFilter=()=>{
+    let filterList=products;
+    if(searchQuery){
+      filterList=filterList.filter((product)=>{
+        product.title.toLowerCase().includes(searchQuery)
+      })
+    }
+    return filterList;
+  }
+  return (
+    <>
+      <Header />
+      <Carousel />
+      <ProductList handleFilter={handleFilter}/>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
