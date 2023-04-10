093febb4a51e90163d7520605e53bf21d11c5249
unknown <phamhuutinh801@gmail.com>
2023-04-10 15:58:17 +0700

    week 5 - initial project

---
+++ Week 5/shopee-nextjs/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+}
+module.exports = nextConfig
+
